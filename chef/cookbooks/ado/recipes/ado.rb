72c40fe6dc37211f209f12ee3fdcecca815c4fc5
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-09-04 23:24:06 +1000

    more robust

--- chef/cookbooks/ado/recipes/ado.rb
+++ chef/cookbooks/ado/recipes/ado.rb
@@ -18,32 +18,31 @@ package "gpg"
 package "podman"
 
 directory "#{node['ado-agent']['agent-download']}"
-directory "#{node['ado-agent']['agent-install']}"
 
 execute 'get-ado-linux-agent' do
-  command [
-    "curl -LO #{node['ado_agent']['vsts_url']}",
-    "tar zxvf #{node['ado_agent']['vsts_file']} --directory #{node['ado-agent']['agent-install']}"
-  ].join(';')
+  command "curl -LO #{node['ado_agent']['vsts_url']}"
   cwd "#{node['ado-agent']['agent-download']}"
+  creates node['ado_agent']['vsts_file']
 end
 
 execute 'get-powershell-core' do
   command "curl -LO #{node['pwsh_url']}"
   cwd "#{node['ado-agent']['agent-download']}"
+  creates node['pwsh_file']
 end
 
 execute 'install-powershell-core' do
   command "#{node['installer']} -i #{node['pwsh_file']}"
   cwd "#{node['ado-agent']['agent-download']}"
+  creates '/usr/bin/pwsh'
 end
 
 if File.exist? "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
   log "databag object is created"
-  databag      = DatabagSecrets.new "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
+  databag = DatabagSecrets.new "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
 else
   log "native databag is created"
-  databag      = data_bag_item("#{node['ado-agent']['databag']}", "#{node['ado-agent']['databagitem']}")
+  databag = data_bag_item("#{node['ado-agent']['databag']}", "#{node['ado-agent']['databagitem']}")
 end
 
 log "organization = #{databag.organization}"
@@ -51,17 +50,45 @@ log "pool         = #{databag.pool}"
 
 # might have an already running agent
 # stop, remove then reconfigure
+execute 'stop-ado-agent-svc' do
+  command "./svc.sh stop"
+  cwd "#{node['ado-agent']['agent-install']}"
+  ignore_failure true
+end
+
+execute 'uninstall-ado-agent-svc' do
+  command "./svc.sh uninstall"
+  cwd "#{node['ado-agent']['agent-install']}"
+  ignore_failure true
+end
+
+# might have an already running agent
+# stop, remove then reconfigure
 execute 'unconfigure-ado-agent' do
   command [
-    "./config.sh",
-    "remove",
-    "--unattended",
-    "--auth pat",
-    "--token #{databag.pat}"
-  ].join(" ")
+            "./config.sh",
+            "remove",
+            "--unattended",
+            "--auth pat",
+            "--token #{databag.pat}"
+          ].join(" ")
   cwd "#{node['ado-agent']['agent-install']}"
-  returns [0, 1] # allow fail
   user node['ado-agent']['agent-user']
+  ignore_failure true
+end
+
+directory "#{node['ado-agent']['agent-install']}" do
+  action :delete
+  recursive true
+end
+
+directory "#{node['ado-agent']['agent-install']}"
+
+execute 'unpack-ado-linux-agent' do
+  command [
+            "tar zxvf #{node['ado_agent']['vsts_file']} --directory #{node['ado-agent']['agent-install']}"
+          ].join(';')
+  cwd "#{node['ado-agent']['agent-download']}"
 end
 
 execute 'configure-ado-agent' do
