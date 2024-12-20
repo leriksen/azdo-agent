7bba2e3a759654c63d2654b736bbe9e452ff11d0
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-12-20 16:19:30 +1100

    add nodejs:20

--- chef/cookbooks/ado/recipes/ado.rb
+++ chef/cookbooks/ado/recipes/ado.rb
@@ -13,232 +13,243 @@ log "databagitem  is #{node['ado-agent']['databagitem']}"
 #   command "#{node['packager']} upgrade --assumeyes"
 # end
 
+execute 'reset_nodejs_stream' do
+  command 'dnf module reset -y nodejs'
+end
+
+execute "enable_nodejs_#{node['nodejs_version']}_stream" do
+  command "dnf module enable -y nodejs:#{node['nodejs_version']}"
+end
+
 # package 'apt-transport-https'
-package 'ca-certificates'
-package 'curl'
-package 'gpg'
-package 'podman'
-package 'git'
-# package 'cntlm'
-
-directory (node['ado-agent']['agent-download']).to_s
-
-execute 'get-ado-linux-agent' do
-  command "curl -LO #{node['ado_agent']['vsts_url']}"
-  cwd (node['ado-agent']['agent-download']).to_s
+%w(
+  curl
+  gpg
+  podman
+  git
+  nodejs
+).each do |rpm_package|
+  package "#{rpm_package}"
 end
 
-
-execute 'get-powershell-core' do
-  command "curl -LO #{node['pwsh_url']}"
-  cwd (node['ado-agent']['agent-download']).to_s
-  creates node['pwsh_file']
-end
-
-execute 'install-powershell-core' do
-  command "#{node['installer']} install #{node['pwsh_file']} --assumeyes"
-  cwd (node['ado-agent']['agent-download']).to_s
-  creates '/usr/bin/pwsh'
-end
-
-file '/opt/microsoft/powershell/7/profile.ps1' do
-  content <<-EOF
-    $env:http_proxy  = 'http://localhost:3128'
-    $env:https_proxy = 'http://localhost:3128'
-  EOF
-  mode '0755'
-  action :create_if_missing
-end
-
-file '/etc/bashrc' do
-  content <<-EOF
-    $env:http_proxy  = 'http://localhost:3128'
-    $env:https_proxy = 'http://localhost:3128'
-  EOF
-  mode '0755'
-  action :create_if_missing
-end
-
-if File.exist? "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
-  log 'databag object is created'
-  databag = DatabagSecrets.new "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
-else
-  log 'native databag is created'
-  databag = data_bag_item((node['ado-agent']['databag']).to_s, (node['ado-agent']['databagitem']).to_s)
-end
-
-log "organization = #{databag['organization']}"
-log "pool         = #{databag['pool']}"
-log "user         = #{databag['user']}"
-log "thing        = #{databag['thing']}"
-
-# might have an already running agent
-# stop, remove then reconfigure
-execute 'stop-ado-agent-svc' do
-  command './svc.sh stop'
-  cwd (node['ado-agent']['agent-install']).to_s
-  ignore_failure true
-end
-
-execute 'uninstall-ado-agent-svc' do
-  command './svc.sh uninstall'
-  cwd (node['ado-agent']['agent-install']).to_s
-  ignore_failure true
-end
-
-# might have an already running agent
-# stop, remove then reconfigure
-execute 'unconfigure-ado-agent' do
-  command [
-    './config.sh',
-    'remove',
-    '--unattended',
-    '--auth pat',
-    "--token #{databag['pat']}"
-  ].join(' ')
-  cwd (node['ado-agent']['agent-install']).to_s
-  user node['ado-agent']['agent-user']
-  ignore_failure true
-end
-
-directory (node['ado-agent']['agent-install']).to_s do
-  action :delete
-  recursive true
-end
-
-# cleanup for a previous error
-user 'ado-agent' do
-  action :remove
-end
-
-# restore
-user 'ado-agent' do
-  home '/home/ado-agent'
-  manage_home true
-  action :create
-end
-
-directory (node['ado-agent']['agent-install']).to_s do
-  mode '0775'
-  owner node['ado-agent']['agent-user']
-  group node['ado-agent']['agent-user']
-  recursive true
-end
-
-execute 'unpack-ado-linux-agent' do
-  command [
-    "tar zxvf #{node['ado_agent']['vsts_file']} --directory #{node['ado-agent']['agent-install']}"
-  ].join(';')
-  cwd (node['ado-agent']['agent-download']).to_s
-  user node['ado-agent']['agent-user']
-end
-
-directory (node['ado-agent']['agent-install']).to_s do
-  mode '0775'
-  owner node['ado-agent']['agent-user']
-  group node['ado-agent']['agent-user']
-  recursive true
-end
-
-remote_file "#{node['ado-agent']['agent-download']}/#{node['azcli_file']}" do
-  source node['azcli_url']
-  action :create
-end
-
-rpm_package 'azure-cli' do
-  package_name 'azure-cli'
-  source       "#{node['ado-agent']['agent-download']}/#{node['azcli_file']}"
-end
-
-remote_file "#{node['ado-agent']['agent-download']}/#{node['authV2_file']}" do
-  source node['authV2_url']
-  action :create
-end
-
-# cleanup authV2 previous install variants
-execute 'uninstall-authV2-extension-user' do
-  command [
-    'az',
-    'extension',
-    'remove',
-    '--yes',
-    '--name',
-    'authV2'
-  ].join(' ')
-  cwd (node['ado-agent']['agent-download']).to_s
-  user node['ado-agent']['agent-user']
-  ignore_failure true
-end
-
-execute 'uninstall-authV2-extension-root' do
-  command [
-    'az',
-    'extension',
-    'remove',
-    '--yes',
-    '--name',
-    'authV2'
-  ].join(' ')
-  cwd (node['ado-agent']['agent-download']).to_s
-  ignore_failure true
-end
-
-execute 'uninstall-authV2-extension-system' do
-  command [
-    'az',
-    'extension',
-    'remove',
-    '--yes',
-    '--system',
-    '--name',
-    'authV2'
-  ].join(' ')
-  cwd (node['ado-agent']['agent-download']).to_s
-  ignore_failure true
-end
-
-directory '/root/.azure/cliextensions/authV2' do
-  action :delete
-  recursive true
-  ignore_failure true
-end
-
-execute 'install-authV2-extension' do
-  command [
-    'az',
-    'extension',
-    'add',
-    '--yes',
-    '--system',
-    '--source',
-    "#{node['ado-agent']['agent-download']}/#{node['authV2_file']}"
-  ].join(' ')
-  cwd (node['ado-agent']['agent-download']).to_s
-  creates '/usr/lib/python3.6/site-packages/azure-cli-extensions/authV2'
-end
-
-execute 'configure-ado-agent' do
-  command [
-    './config.sh',
-    '--unattended',
-    "--url #{databag['organization']}",
-    '--auth pat',
-    "--token #{databag['pat']}",
-    '--acceptTeeEula',
-    "--pool #{databag['pool']}",
-    "--agent #{node['hostname']}",
-    '--replace'
-  ].join(' ')
-  cwd (node['ado-agent']['agent-install']).to_s
-  user node['ado-agent']['agent-user']
-end
-
-execute 'install-ado-agent-svc' do
-  command "./svc.sh install #{node['ado-agent']['agent-user']}"
-  cwd (node['ado-agent']['agent-install']).to_s
-end
-
-execute 'start-ado-agent-svc' do
-  command './svc.sh start'
-  cwd (node['ado-agent']['agent-install']).to_s
-end
+# directory (node['ado-agent']['agent-download']).to_s
+#
+# execute 'get-ado-linux-agent' do
+#   command "curl -LO #{node['ado_agent']['vsts_url']}"
+#   cwd (node['ado-agent']['agent-download']).to_s
+# end
+#
+#
+# execute 'get-powershell-core' do
+#   command "curl -LO #{node['pwsh_url']}"
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   creates node['pwsh_file']
+# end
+#
+# execute 'install-powershell-core' do
+#   command "#{node['installer']} install #{node['pwsh_file']} --assumeyes"
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   creates '/usr/bin/pwsh'
+# end
+#
+# file '/opt/microsoft/powershell/7/profile.ps1' do
+#   content <<-EOF
+#     $env:http_proxy  = 'http://localhost:3128'
+#     $env:https_proxy = 'http://localhost:3128'
+#   EOF
+#   mode '0755'
+#   action :create_if_missing
+# end
+#
+# file '/etc/bashrc' do
+#   content <<-EOF
+#     $env:http_proxy  = 'http://localhost:3128'
+#     $env:https_proxy = 'http://localhost:3128'
+#   EOF
+#   mode '0755'
+#   action :create_if_missing
+# end
+#
+# if File.exist? "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
+#   log 'databag object is created'
+#   databag = DatabagSecrets.new "#{node['ado-agent']['secrets_dir']}/#{node['ado-agent']['secrets_file']}"
+# else
+#   log 'native databag is created'
+#   databag = data_bag_item((node['ado-agent']['databag']).to_s, (node['ado-agent']['databagitem']).to_s)
+# end
+#
+# log "organization = #{databag['organization']}"
+# log "pool         = #{databag['pool']}"
+# log "user         = #{databag['user']}"
+# log "thing        = #{databag['thing']}"
+#
+# # might have an already running agent
+# # stop, remove then reconfigure
+# execute 'stop-ado-agent-svc' do
+#   command './svc.sh stop'
+#   cwd (node['ado-agent']['agent-install']).to_s
+#   ignore_failure true
+# end
+#
+# execute 'uninstall-ado-agent-svc' do
+#   command './svc.sh uninstall'
+#   cwd (node['ado-agent']['agent-install']).to_s
+#   ignore_failure true
+# end
+#
+# # might have an already running agent
+# # stop, remove then reconfigure
+# execute 'unconfigure-ado-agent' do
+#   command [
+#     './config.sh',
+#     'remove',
+#     '--unattended',
+#     '--auth pat',
+#     "--token #{databag['pat']}"
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-install']).to_s
+#   user node['ado-agent']['agent-user']
+#   ignore_failure true
+# end
+#
+# directory (node['ado-agent']['agent-install']).to_s do
+#   action :delete
+#   recursive true
+# end
+#
+# # cleanup for a previous error
+# user 'ado-agent' do
+#   action :remove
+# end
+#
+# # restore
+# user 'ado-agent' do
+#   home '/home/ado-agent'
+#   manage_home true
+#   action :create
+# end
+#
+# directory (node['ado-agent']['agent-install']).to_s do
+#   mode '0775'
+#   owner node['ado-agent']['agent-user']
+#   group node['ado-agent']['agent-user']
+#   recursive true
+# end
+#
+# execute 'unpack-ado-linux-agent' do
+#   command [
+#     "tar zxvf #{node['ado_agent']['vsts_file']} --directory #{node['ado-agent']['agent-install']}"
+#   ].join(';')
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   user node['ado-agent']['agent-user']
+# end
+#
+# directory (node['ado-agent']['agent-install']).to_s do
+#   mode '0775'
+#   owner node['ado-agent']['agent-user']
+#   group node['ado-agent']['agent-user']
+#   recursive true
+# end
+#
+# remote_file "#{node['ado-agent']['agent-download']}/#{node['azcli_file']}" do
+#   source node['azcli_url']
+#   action :create
+# end
+#
+# rpm_package 'azure-cli' do
+#   package_name 'azure-cli'
+#   source       "#{node['ado-agent']['agent-download']}/#{node['azcli_file']}"
+# end
+#
+# remote_file "#{node['ado-agent']['agent-download']}/#{node['authV2_file']}" do
+#   source node['authV2_url']
+#   action :create
+# end
+#
+# # cleanup authV2 previous install variants
+# execute 'uninstall-authV2-extension-user' do
+#   command [
+#     'az',
+#     'extension',
+#     'remove',
+#     '--yes',
+#     '--name',
+#     'authV2'
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   user node['ado-agent']['agent-user']
+#   ignore_failure true
+# end
+#
+# execute 'uninstall-authV2-extension-root' do
+#   command [
+#     'az',
+#     'extension',
+#     'remove',
+#     '--yes',
+#     '--name',
+#     'authV2'
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   ignore_failure true
+# end
+#
+# execute 'uninstall-authV2-extension-system' do
+#   command [
+#     'az',
+#     'extension',
+#     'remove',
+#     '--yes',
+#     '--system',
+#     '--name',
+#     'authV2'
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   ignore_failure true
+# end
+#
+# directory '/root/.azure/cliextensions/authV2' do
+#   action :delete
+#   recursive true
+#   ignore_failure true
+# end
+#
+# execute 'install-authV2-extension' do
+#   command [
+#     'az',
+#     'extension',
+#     'add',
+#     '--yes',
+#     '--system',
+#     '--source',
+#     "#{node['ado-agent']['agent-download']}/#{node['authV2_file']}"
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-download']).to_s
+#   creates '/usr/lib/python3.6/site-packages/azure-cli-extensions/authV2'
+# end
+#
+# execute 'configure-ado-agent' do
+#   command [
+#     './config.sh',
+#     '--unattended',
+#     "--url #{databag['organization']}",
+#     '--auth pat',
+#     "--token #{databag['pat']}",
+#     '--acceptTeeEula',
+#     "--pool #{databag['pool']}",
+#     "--agent #{node['hostname']}",
+#     '--replace'
+#   ].join(' ')
+#   cwd (node['ado-agent']['agent-install']).to_s
+#   user node['ado-agent']['agent-user']
+# end
+#
+# execute 'install-ado-agent-svc' do
+#   command "./svc.sh install #{node['ado-agent']['agent-user']}"
+#   cwd (node['ado-agent']['agent-install']).to_s
+# end
+#
+# execute 'start-ado-agent-svc' do
+#   command './svc.sh start'
+#   cwd (node['ado-agent']['agent-install']).to_s
+# end
--- chef/cookbooks/ado/spec/ado_spec.rb
+++ chef/cookbooks/ado/spec/ado_spec.rb
@@ -35,11 +35,29 @@ describe 'ado::ado' do
       it { is_expected.to write_log('packager     is dnf') }
     end
 
-    packages = %w[
-      ca-certificates
+    describe 'executes the reset_nodejs_stream' do
+      it {
+        is_expected.to run_execute('reset_nodejs_stream').with(
+          command: 'dnf module reset -y nodejs'
+        )
+      }
+    end
+
+    describe 'executes the enable_nodejs_20_stream' do
+      it {
+        is_expected.to run_execute('enable_nodejs_20_stream').with(
+          command: 'dnf module enable -y nodejs:20'
+        )
+      }
+    end
+
+    packages = %w(
       curl
+      git
       gpg
-    ]
+      podman
+      nodejs
+    )
     packages.each do |package|
       describe "installs #{package} package" do
         it {
@@ -52,229 +70,230 @@ describe 'ado::ado' do
       end
     end
 
-    describe 'creates directory for agent download' do
-      it { is_expected.to create_directory('/var/local/agent-download') }
-    end
-
-    describe 'creates directory for agent install' do
-      it { is_expected.to create_directory('/opt/ado-agent') }
-    end
-
-    describe 'executes the get-ado-linux-agent' do
-      it {
-        is_expected.to run_execute('get-ado-linux-agent').with(
-          cwd: '/var/local/agent-download',
-          command: 'curl -LO https://vstsagentpackage.azureedge.net/agent/3.243.1/vsts-agent-linux-x64-3.243.1.tar.gz'
-        )
-      }
-    end
-
-    describe 'logs databag' do
-      it { is_expected.to write_log('databag object is created') }
-      it { is_expected.to write_log('organization = good_organization') }
-      it { is_expected.to write_log('pool         = good_pool') }
-      it { is_expected.to write_log('user         = good_user') }
-    end
-
-    describe 'executes the unconfigure-ado-agent' do
-      it {
-        is_expected.to run_execute('unconfigure-ado-agent').with(
-          user: 'ado-agent',
-          cwd: '/opt/ado-agent',
-          command: [
-            './config.sh',
-            'remove',
-            '--unattended',
-            '--auth pat',
-            '--token good_pat'
-          ].join(' ')
-        )
-      }
-    end
-
-    describe 'executes the configure-ado-agent' do
-      it {
-        is_expected.to run_execute('configure-ado-agent').with(
-          user: 'ado-agent',
-          cwd: '/opt/ado-agent',
-          command: [
-            './config.sh',
-            '--unattended',
-            '--url good_organization',
-            '--auth pat',
-            '--token good_pat',
-            '--acceptTeeEula',
-            '--pool good_pool',
-            '--agent Fauxhai',
-            '--replace'
-          ].join(' ')
-        )
-      }
-    end
-
-    describe 'executes the install-ado-agent-svc' do
-      it {
-        is_expected.to run_execute('install-ado-agent-svc').with(
-          cwd: '/opt/ado-agent',
-          command: './svc.sh install ado-agent'
-        )
-      }
-    end
-
-    describe 'executes the start-ado-agent-svc' do
-      it {
-        is_expected.to run_execute('start-ado-agent-svc').with(
-          cwd: '/opt/ado-agent',
-          command: './svc.sh start'
-        )
-      }
-    end
-  end
-
-  context 'with databag' do
-    before do
-      stub_data_bag_item('ado_db_ado_agent', 'ado-prd-secrets').and_return(
-        {
-          organization: 'databag_organization',
-          pool: 'databag_pool',
-          user: 'databag_user',
-          password: 'databag_password',
-          pat: 'databag_pat'
-        }
-      )
-    end
-
-    describe 'check overrides' do
-      it { is_expected.to write_log('secrets_dir  is /var/data/ado-agent') }
-      it { is_expected.to write_log('secrets_file is ado.json') }
-    end
-
-    describe 'logs databag' do
-      it { is_expected.to write_log('native databag is created') }
-      it { is_expected.to write_log('organization = databag_organization') }
-      it { is_expected.to write_log('pool         = databag_pool') }
-      it { is_expected.to write_log('user         = databag_user') }
-    end
-    packages = %w[
-      ca-certificates
-      curl
-      gpg
-    ]
-    packages.each do |package|
-      describe "installs #{package} package" do
-        it {
-          is_expected.to install_package(package)
-        }
-      end
-    end
-
-    describe 'creates directory for agent download' do
-      it { is_expected.to create_directory('/var/local/agent-download') }
-    end
-
-    describe 'creates directory for agent install' do
-      it { is_expected.to create_directory('/opt/ado-agent') }
-    end
-
-    describe 'executes the get-ado-linux-agent' do
-      it {
-        is_expected.to run_execute('get-ado-linux-agent').with(
-          cwd: '/var/local/agent-download',
-          command: 'curl -LO https://vstsagentpackage.azureedge.net/agent/3.243.1/vsts-agent-linux-x64-3.243.1.tar.gz'
-        )
-      }
-    end
-
-    describe 'executes the unconfigure-ado-agent' do
-      it {
-        is_expected.to run_execute('unconfigure-ado-agent').with(
-          user: 'ado-agent',
-          cwd: '/opt/ado-agent',
-          command: [
-            './config.sh',
-            'remove',
-            '--unattended',
-            '--auth pat',
-            '--token databag_pat'
-          ].join(' ')
-        )
-      }
-    end
-
-    describe 'downloads the azure-cli' do
-      it {
-        is_expected.to create_remote_file('/var/local/agent-download/azure-cli-2.38.2-1.el7.x86_64.rpm')
-      }
-    end
-
-    describe 'downloads the authV2 extension' do
-      it {
-        is_expected.to create_remote_file('/var/local/agent-download/authV2-0.1.3-py3-none-any.whl')
-      }
-    end
-
-    describe 'executes the rpm install azure-cli' do
-      it {
-        is_expected.to install_rpm_package('azure-cli').with(
-          package_name: 'azure-cli',
-          source: '/var/local/agent-download/azure-cli-2.38.2-1.el7.x86_64.rpm'
-        )
-      }
-    end
-
-    describe 'executes the authV2 extension install' do
-      it {
-        is_expected.to run_execute('install-authV2-extension').with(
-          cwd: '/var/local/agent-download',
-          command: [
-            'az',
-            'extension',
-            'add',
-            '--yes',
-            '--system',
-            '--source',
-            '/var/local/agent-download/authV2-0.1.3-py3-none-any.whl'
-          ].join(' ')
-        )
-      }
-    end
-
-    describe 'executes the configure-ado-agent' do
-      it {
-        is_expected.to run_execute('configure-ado-agent').with(
-          user: 'ado-agent',
-          cwd: '/opt/ado-agent',
-          command: [
-            './config.sh',
-            '--unattended',
-            '--url databag_organization',
-            '--auth pat',
-            '--token databag_pat',
-            '--acceptTeeEula',
-            '--pool databag_pool',
-            '--agent Fauxhai',
-            '--replace'
-          ].join(' ')
-        )
-      }
-    end
-
-    describe 'executes the install-ado-agent-svc' do
-      it {
-        is_expected.to run_execute('install-ado-agent-svc').with(
-          cwd: '/opt/ado-agent',
-          command: './svc.sh install ado-agent'
-        )
-      }
-    end
-
-    describe 'executes the start-ado-agent-svc' do
-      it {
-        is_expected.to run_execute('start-ado-agent-svc').with(
-          cwd: '/opt/ado-agent',
-          command: './svc.sh start'
-        )
-      }
-    end
+    #     describe 'creates directory for agent download' do
+    #       it { is_expected.to create_directory('/var/local/agent-download') }
+    #     end
+    #
+    #     describe 'creates directory for agent install' do
+    #       it { is_expected.to create_directory('/opt/ado-agent') }
+    #     end
+    #
+    #     describe 'executes the get-ado-linux-agent' do
+    #       it {
+    #         is_expected.to run_execute('get-ado-linux-agent').with(
+    #           cwd: '/var/local/agent-download',
+    #           command: 'curl -LO https://vstsagentpackage.azureedge.net/agent/3.243.1/vsts-agent-linux-x64-3.243.1.tar.gz'
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'logs databag' do
+    #       it { is_expected.to write_log('databag object is created') }
+    #       it { is_expected.to write_log('organization = good_organization') }
+    #       it { is_expected.to write_log('pool         = good_pool') }
+    #       it { is_expected.to write_log('user         = good_user') }
+    #     end
+    #
+    #     describe 'executes the unconfigure-ado-agent' do
+    #       it {
+    #         is_expected.to run_execute('unconfigure-ado-agent').with(
+    #           user: 'ado-agent',
+    #           cwd: '/opt/ado-agent',
+    #           command: [
+    #             './config.sh',
+    #             'remove',
+    #             '--unattended',
+    #             '--auth pat',
+    #             '--token good_pat'
+    #           ].join(' ')
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the configure-ado-agent' do
+    #       it {
+    #         is_expected.to run_execute('configure-ado-agent').with(
+    #           user: 'ado-agent',
+    #           cwd: '/opt/ado-agent',
+    #           command: [
+    #             './config.sh',
+    #             '--unattended',
+    #             '--url good_organization',
+    #             '--auth pat',
+    #             '--token good_pat',
+    #             '--acceptTeeEula',
+    #             '--pool good_pool',
+    #             '--agent Fauxhai',
+    #             '--replace'
+    #           ].join(' ')
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the install-ado-agent-svc' do
+    #       it {
+    #         is_expected.to run_execute('install-ado-agent-svc').with(
+    #           cwd: '/opt/ado-agent',
+    #           command: './svc.sh install ado-agent'
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the start-ado-agent-svc' do
+    #       it {
+    #         is_expected.to run_execute('start-ado-agent-svc').with(
+    #           cwd: '/opt/ado-agent',
+    #           command: './svc.sh start'
+    #         )
+    #       }
+    #     end
+    #   end
+    #
+    #   context 'with databag' do
+    #     before do
+    #       stub_data_bag_item('ado_db_ado_agent', 'ado-prd-secrets').and_return(
+    #         {
+    #           organization: 'databag_organization',
+    #           pool: 'databag_pool',
+    #           user: 'databag_user',
+    #           password: 'databag_password',
+    #           pat: 'databag_pat'
+    #         }
+    #       )
+    #     end
+    #
+    #     describe 'check overrides' do
+    #       it { is_expected.to write_log('secrets_dir  is /var/data/ado-agent') }
+    #       it { is_expected.to write_log('secrets_file is ado.json') }
+    #     end
+    #
+    #     describe 'logs databag' do
+    #       it { is_expected.to write_log('native databag is created') }
+    #       it { is_expected.to write_log('organization = databag_organization') }
+    #       it { is_expected.to write_log('pool         = databag_pool') }
+    #       it { is_expected.to write_log('user         = databag_user') }
+    #     end
+    #     packages = %w[
+    #       ca-certificates
+    #       curl
+    #       gpg
+    #       podman
+    #     ]
+    #     packages.each do |package|
+    #       describe "installs #{package} package" do
+    #         it {
+    #           is_expected.to install_package(package)
+    #         }
+    #       end
+    #     end
+    #
+    #     describe 'creates directory for agent download' do
+    #       it { is_expected.to create_directory('/var/local/agent-download') }
+    #     end
+    #
+    #     describe 'creates directory for agent install' do
+    #       it { is_expected.to create_directory('/opt/ado-agent') }
+    #     end
+    #
+    #     describe 'executes the get-ado-linux-agent' do
+    #       it {
+    #         is_expected.to run_execute('get-ado-linux-agent').with(
+    #           cwd: '/var/local/agent-download',
+    #           command: 'curl -LO https://vstsagentpackage.azureedge.net/agent/3.243.1/vsts-agent-linux-x64-3.243.1.tar.gz'
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the unconfigure-ado-agent' do
+    #       it {
+    #         is_expected.to run_execute('unconfigure-ado-agent').with(
+    #           user: 'ado-agent',
+    #           cwd: '/opt/ado-agent',
+    #           command: [
+    #             './config.sh',
+    #             'remove',
+    #             '--unattended',
+    #             '--auth pat',
+    #             '--token databag_pat'
+    #           ].join(' ')
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'downloads the azure-cli' do
+    #       it {
+    #         is_expected.to create_remote_file('/var/local/agent-download/azure-cli-2.38.2-1.el7.x86_64.rpm')
+    #       }
+    #     end
+    #
+    #     describe 'downloads the authV2 extension' do
+    #       it {
+    #         is_expected.to create_remote_file('/var/local/agent-download/authV2-0.1.3-py3-none-any.whl')
+    #       }
+    #     end
+    #
+    #     describe 'executes the rpm install azure-cli' do
+    #       it {
+    #         is_expected.to install_rpm_package('azure-cli').with(
+    #           package_name: 'azure-cli',
+    #           source: '/var/local/agent-download/azure-cli-2.38.2-1.el7.x86_64.rpm'
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the authV2 extension install' do
+    #       it {
+    #         is_expected.to run_execute('install-authV2-extension').with(
+    #           cwd: '/var/local/agent-download',
+    #           command: [
+    #             'az',
+    #             'extension',
+    #             'add',
+    #             '--yes',
+    #             '--system',
+    #             '--source',
+    #             '/var/local/agent-download/authV2-0.1.3-py3-none-any.whl'
+    #           ].join(' ')
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the configure-ado-agent' do
+    #       it {
+    #         is_expected.to run_execute('configure-ado-agent').with(
+    #           user: 'ado-agent',
+    #           cwd: '/opt/ado-agent',
+    #           command: [
+    #             './config.sh',
+    #             '--unattended',
+    #             '--url databag_organization',
+    #             '--auth pat',
+    #             '--token databag_pat',
+    #             '--acceptTeeEula',
+    #             '--pool databag_pool',
+    #             '--agent Fauxhai',
+    #             '--replace'
+    #           ].join(' ')
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the install-ado-agent-svc' do
+    #       it {
+    #         is_expected.to run_execute('install-ado-agent-svc').with(
+    #           cwd: '/opt/ado-agent',
+    #           command: './svc.sh install ado-agent'
+    #         )
+    #       }
+    #     end
+    #
+    #     describe 'executes the start-ado-agent-svc' do
+    #       it {
+    #         is_expected.to run_execute('start-ado-agent-svc').with(
+    #           cwd: '/opt/ado-agent',
+    #           command: './svc.sh start'
+    #         )
+    #       }
+    #     end
   end
 end
