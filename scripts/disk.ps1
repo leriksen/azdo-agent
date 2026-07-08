b027a62db5c5b37280445626e278d74b19f0765b
Leif Eriksen <leif.eriksen.au@gmail.com>
2026-07-08 17:41:47 +1000

    disk

--- scripts/disk.ps1
+++ scripts/disk.ps1
@@ -4,6 +4,7 @@
         Select-Object Name,
             @{N='Used(GB)';E={[math]::Round($_.Used/1GB,2)}},
             @{N='Free(GB)';E={[math]::Round($_.Free/1GB,2)}},
-            @{N='Total(GB)';E={[math]::Round(($_.Used+$_.Free)/1GB,2)}} |
+            @{N='Total(GB)';E={[math]::Round(($_.Used+$_.Free)/1GB,2)}},
+            @{N='Used(%)';E={[math]::Round($_.Used/($_.Used+$_.Free)*100,1)}} |
         Format-Table -AutoSize
   displayName: 'Show disk space'
